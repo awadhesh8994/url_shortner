a78d9300b696d20e32084e8f9db2556b3f23c65f
Awadhesh Kumar <awadhesh23th@gmail.com>
2025-03-11 22:23:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,4 +20,4 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-module.exports = app; // ✅ This is important for Vercel deployment
+module.exports = app; 
